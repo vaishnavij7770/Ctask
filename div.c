3c2b729d5b05977405d0e4747f45c442af03b8b0
Vaishnavi <vaishuj7770@gmail.com>
2026-04-23 11:19:46 +0530

    Add simple arithmetic C programs
    Add three small C programs (div.c, multi.c, sub.c) that perform division, multiplication, and subtraction with hardcoded operands and print the results. Also include their compiled artifacts (executables, object files, .pdb and .ilk debug files) and an updated vc140.pdb. These appear to be initial additions of example/demo programs and their build outputs.

---
+++ div.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=6;
+    int b=2;
+
+    int c=a/b;
+
+    printf("Division is %d",c);
+}
---
+++ multi.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=7;
+    int b=2;
+
+    int c=a*b;
+
+    printf("Multiplication is %d",c);
+}

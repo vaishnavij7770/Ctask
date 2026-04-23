9d23e740735955689196648c4492a0a07656c1d6
Vaishnavi <vaishuj7770@gmail.com>
2026-04-23 11:39:07 +0530

    task first in C

---
+++ task1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int m=56;
+    printf("This is integer %d", m);
+
+    float n=277.8;
+    printf("\nThis is Float %f", n);
+
+    char o='a';
+    printf("\nThis is character %c", o);
+
+    int p=67;
+    int q;
+    q=p;
+
+    printf("\n%d",q);
+
+    //3 practice programs
+
+    int a=1;
+    int b=0;
+    b=a;
+    printf("\n%d",b);
+
+    int x=11;
+    int z=x+1;
+    printf("%d",z);
+
+    int age = 20;
+    age = age + 1;  
+    printf("\n%d", age);
+
+}

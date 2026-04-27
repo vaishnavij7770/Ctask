c993bf846d930f49bf5b2bbff44bcb9c2d1fcbc1
Vaishnavi <vaishuj7770@gmail.com>
2026-04-27 09:58:10 +0530

    conditional statements part done

--- Day3/condition.c
+++ Day3/condition.c
@@ -1,14 +1,58 @@
 //If else 
 
-#include<stdio.h>
-int main(){
-    int num=50;
-    if(num%2==0){
-        printf("This is even no");
-    }
-    else{
-        printf("This is odd no");
-    }
-}
+// #include<stdio.h>
+// int main(){
+//     int num=50;
+//     if(num%2==0){
+//         printf("This is even no");
+//     }
+//     else{
+//         printf("This is odd no");
+//     }
+// }
 
-//
+//Else if
+// #include<stdio.h>
+// int main(){
+//     int marks;
+//     printf("enter your marks:");
+//     scanf("%d",&marks);
+//     if(marks==70){
+//         printf("average marks");
+//     }
+//     else if(marks==80){
+//         printf("Best marks");
+//     }
+//     else{
+//         printf("invalid marks");
+//     }
+// }
+
+//Switch Case
+
+// #include<stdio.h>
+// int main(){
+//     int time;
+
+//     printf("enter your time:");
+//     scanf("%d",&time);
+
+//     switch (time)
+//     {
+//     case 7:
+//         printf("Good Morning");
+//         break;
+//     case 12:
+//         printf("Good afternoon");
+//         break;
+//     case 16:
+//         printf("Good evening");
+//         break;
+    
+//     default:
+//         printf("good night");
+//         break;
+//     }
+// }
+
+
---
+++ Day3/loop.c
@@ -0,0 +1,29 @@
+//for loop
+// #include<stdio.h>
+// int main(){
+//     for(int i;i<=10;i++){
+//         printf("fortune\n");
+//     }
+// }
+
+//while loop
+
+// #include<stdio.h>
+// int main(){
+//     int a=1;
+//     while(a<=10){
+//         printf("fortune\n");
+//         a++;
+//     }
+// }
+
+//do-while loop
+
+#include<stdio.h>
+int main(){
+    int j=11;
+    do{
+        printf("fortune\n");
+    }
+    while (j<=10);
+}

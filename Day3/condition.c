251051ec07147336e7512b88c478b92792232aba
Vaishnavi <vaishuj7770@gmail.com>
2026-04-27 09:29:48 +0530

    Conditional statement if else added

---
+++ Day3/condition.c
@@ -0,0 +1,14 @@
+//If else 
+
+#include<stdio.h>
+int main(){
+    int num=50;
+    if(num%2==0){
+        printf("This is even no");
+    }
+    else{
+        printf("This is odd no");
+    }
+}
+
+//

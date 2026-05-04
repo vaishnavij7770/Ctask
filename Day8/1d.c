6d6a9413cf606dbd03abe0b8e28a4af294268e12
Vaishnavi <vaishuj7770@gmail.com>
2026-05-04 11:01:36 +0530

    Add Day8 C programs and build artifacts
    Adds Day8 examples: 1d.c (prints elements of an int array using a for loop) and an added 2d.c file. Also commits associated build outputs (1d.exe, 1d.obj, 1d.ilk, 1d.pdb, vc140.pdb). Note: the diff contains compiled binaries and pdb/ilk files generated by the build.

---
+++ Day8/1d.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    // printf("%d\n",a[0]);
+    // printf("%d\n",a[1]);
+    // printf("%d\n",a[2]);
+    // printf("%d\n",a[3]);
+    // printf("%d\n",a[4]);
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    
+}

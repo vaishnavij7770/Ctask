3a35bc5da3d7dc128fa2b701b9c50b9735e7d9bd
Vaishnavi <vaishuj7770@gmail.com>
2026-05-13 15:09:13 +0530

    Updated pattern.c

--- CLogic/pattern.c
+++ CLogic/pattern.c
@@ -2,7 +2,12 @@
 int main(){
     for(int r=0;r<5;r++){
         for(int c=0;c<6;c++){
-              printf("* ");
+            if(r==0 || r==2 || r==4 || r==1&&c==0 || r==3&&c==5){
+                 printf("* ");  
+            }
+            else{
+                printf("  ");
+            }
         }
         printf("\n");
     }
